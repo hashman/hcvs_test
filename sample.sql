e089c8e266adc29b3afe0dc3c10a6de0f3606272
Hashman Lin <coosos810609@gmail.com>
2018-05-26 16:27:37 +0800

    fixup sql

--- sample.sql
+++ sample.sql
@@ -1,17 +1,3 @@
-CREATE DATABASE users;
-
-create table counter (
-    id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    account varchar(100) UNIQUE not null,
-    count int default 0 not null
-);
-
-INSERT INTO count (account, count) VALUES ('hash', 0);
-
-INSERT INTO users (account, password) VALUES ('admin', 123456);
-
--- Create TABLE SQL
-
 CREATE TABLE `users` (
   `id` int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `account` varchar(100) UNIQUE NOT NULL,
